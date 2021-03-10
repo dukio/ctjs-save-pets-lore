36ff6cdfd64641dff872d42279533dc51619102c
Davide Gaio <dggaio31@gmail.com>
2021-03-10 20:20:48 +0100

    bugfix

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import { isSkyblock } from 'dukioooo'
-
 // prettier-ignore
 const petsSlots = [
 	10,11,12,13,14,15,16,
@@ -7,31 +5,25 @@ const petsSlots = [
 	28,29,30,31,32,33,34,
 	37,38,39,40,41,42,43,
 ]
-
 let lastInventory = null
 
 register('guiRender', function () {
-	if (!isSkyblock()) {
-		return
-	}
-
 	const inventory = Player.getOpenedInventory()
 
-	// Caching inventory id to prevent running the code multiple times for the same window
-	if (lastInventory === inventory.getWindowId()) {
-		return
-	}
-	if (
-		inventory.getStackInSlot(49).getName().removeFormatting() !==
-		'tile.air.name'
-	) {
-		lastInventory = inventory.getWindowId()
-	}
-
-	// Checking if it's a calendar window
+	// Checking if it's a /pets window
 	if (!inventory || !/^\(\d\/\d\) Pets$/g.test(inventory.getName())) {
 		return
 	}
+
+	// Caching inventory id to prevent running the code multiple times for the same window
+	if (
+		lastInventory === inventory.getWindowId() ||
+		inventory.getStackInSlot(49).getName().removeFormatting() ===
+			'tile.air.name'
+	) {
+		return
+	}
+	lastInventory = inventory.getWindowId()
 
 	petsSlots.forEach((slotId) => {
 		const item = inventory.getStackInSlot(slotId)
